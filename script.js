a7839201050a679094734e50180dbb914ef057a1
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-02-08 15:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,11 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+	let count=0;
+	for(let i=0; i<n; i++){
+		count++;
+	}
+	return count;
 };
 
 // Do not change the code below
